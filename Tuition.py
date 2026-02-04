28d7f4b55e68749913d548235dce872770b0c2dd
awrigh69 <awrigh69@students.ChattahoocheeTech.edu>
2026-02-04 13:38:52 -0500

    Remove comments and clean up Tuition.py

--- Tuition.py
+++ Tuition.py
@@ -1,13 +1,3 @@
-# Class: CIST 2742 Python Programming I
-# Term: Spring 2026
-# Instructor: Chris Bishop
-# Description: Solution to Lab # 3 Tuition
-# Author: Andrew Wright
-#
-# By turning in this code, I Pledge:
-# 1. That I have completed the programming assignment independently.
-# 2. I have not copied the code from a student or any source.
-# 3. I have not given my code to any student.
 
 base_year = 1
 
@@ -20,5 +10,6 @@ while base_year <= years_input:
     curr_tuition += curr_tuition * percent
 
     print(f"The tuition for year {base_year} is: ${curr_tuition:,.2f}")
+
 
     base_year += 1
